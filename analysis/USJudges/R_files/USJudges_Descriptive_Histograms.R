b083dcdb3c304b15be733f4763ab4a3c0c7a62de
fabianbohnenberger <fabian.bohnenberger@onlinehome.de>
2015-10-01 11:53:23 +0200

    more on USJudges

---
+++ analysis/USJudges/R_files/USJudges_Descriptive_Histograms.R
@@ -0,0 +1,19 @@
+#############################
+# Histograms for USJudgeRatings
+# Fabian Bohnenberger
+# for further information see: USJudges_RMarkdown.Rmd file 
+#############################
+
+
+## Histogram 
+hist(USJudgeRatings$INTG)
+hist(USJudgeRatings$PREP) 
+hist(USJudgeRatings$ORAL) 
+hist(USJudgeRatings$PHYS) 
+
+
+
+
+
+
+
